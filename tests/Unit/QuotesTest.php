e4c941343dd9bc286b24981b73c6a1c7acbf260e
Bajomo David Mayowa <bajomomayowa@gmail.com>
2022-09-30 15:51:39 +0100

    Updated readme

--- tests/Unit/QuotesTest.php
+++ tests/Unit/QuotesTest.php
@@ -4,10 +4,11 @@ namespace Tests\Unit;
 
 use App\Helpers\PullCompanyJson;
 use App\Helpers\PullCompanyQuotes;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class QuotesTest extends TestCase
 {
+
     public function test_check_quotes_return_array()
     {
         $quotes = new PullCompanyQuotes('AIIT', '09/01/2016', '09/10/2022');
